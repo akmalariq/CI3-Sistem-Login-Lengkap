7983b1aa4f7387e96b400049f78d72ccd22d77ea
akmalariq <santoso54321@gmail.com>
2021-05-10 13:12:27 +0700

    add registration feature

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -20,10 +20,18 @@ class Auth extends CI_Controller
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
 	// Call View Function to show Login Page
 	public function index()
 	{
-		$this->load->view('templates/auth_header');
+		$data['title'] = "Sistem Login";
+
+		$this->load->view('templates/auth_header', $data);
 		$this->load->view('auth/login');
 		$this->load->view('templates/auth_footer');
 	}
@@ -31,8 +39,39 @@ class Auth extends CI_Controller
 	// Call View Function to show Registration Page
 	public function registration()
 	{
-		$this->load->view('templates/auth_header');
-		$this->load->view('auth/registration');
-		$this->load->view('templates/auth_footer');
+		// Check the inputs if valid
+		$this->form_validation->set_rules('name', 'Name', 'required|trim');
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+			'is_unique' => "Email has already been registered"
+		]);
+		$this->form_validation->set_rules('password1', 'Password1', 'required|trim|min_length[3]|matches[password2]', [
+			'matches' => "Password didn't match!",
+			'min_length' => "Password too short!"
+		]);
+		$this->form_validation->set_rules('password2', 'Password2', 'required|trim|matches[password2]');
+
+		if ($this->form_validation->run() == false) {
+
+			$data['title'] = "Sistem Registration";
+
+			$this->load->view('templates/auth_header', $data);
+			$this->load->view('auth/registration');
+			$this->load->view('templates/auth_footer');
+		} else {
+			$data = [
+				'name' => htmlspecialchars($this->input->post('name', true)),
+				'email' => htmlspecialchars($this->input->post('email', true)),
+				'image' => "default.jpg",
+				'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+				'role_id' => 2,
+				'is_active' => 1,
+				'date_created' => time()
+
+			];
+
+			$this->db->insert('user', $data);
+			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Pleas Login </div>');
+			redirect('auth');
+		}
 	}
 }
