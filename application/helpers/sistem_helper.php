f3dc9da0183f26c6086957d4240812ba454b0bba
akmalariq <santoso54321@gmail.com>
2021-05-18 15:26:37 +0700

    access management

--- application/helpers/sistem_helper.php
+++ application/helpers/sistem_helper.php
@@ -22,3 +22,21 @@ function is_logged_in()
         }
     }
 }
+
+function check_access($role_id, $menu_id)
+{
+    $ci = get_instance();
+
+    // $ci->db->where('role_id', $role_id);
+    // $ci->db->where('menu_id', $menu_id);
+    // $ci->db->get('user_access_menu');
+
+    $result = $ci->db->get_where('user_access_menu', [
+        'role_id' => $role_id,
+        'menu_id' => $menu_id
+    ]);
+
+    if ($result->num_rows() > 0) {
+        return "checked='checked'";
+    }
+}
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -49,14 +49,14 @@
                 <?php else : ?>
                 <li class="nav-item">
                 <?php endif; ?>
-                <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+                <a class="nav-link pb-0" href="<?= base_url($sm['url']); ?>">
                     <i class="<?= $sm['icon']; ?>"></i>
                     <span><?= $sm['title']; ?></span></a>
                 </li>
             <?php endforeach; ?>
 
             <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
+            <hr class="sidebar-divider mt-3">
         <?php endforeach; ?>
 
         <!-- Nav Item - Logout -->
